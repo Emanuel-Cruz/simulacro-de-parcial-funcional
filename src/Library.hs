60db45bde90a9959057604c0113d38c1ebba2bdb
Emanuel Cruz <emacruz@frba.utn.edu.ar>
2024-06-04 09:17:09 -0300

    modelado de persona y parque

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,24 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+-- SIMULACRO PARCIAL
+-- ---------------------------------------------------
+
+data Persona = Persona
+  { nombre :: String,
+    nivelOrientacion :: Number,
+    nivelDeEmocion :: Number,
+    nivelDeCultura :: Number
+  }
+  deriving (Show)
+
+isabel = Persona {nombre = "isabel", nivelOrientacion = 100, nivelDeEmocion = 100, nivelDeCultura = 100}
+
+mario = Persona {nombre = "mario", nivelOrientacion = 50, nivelDeEmocion = 150, nivelDeCultura = 30}
+
+teresa = Persona {nombre = "teresa", nivelOrientacion = 0, nivelDeEmocion = 0, nivelDeCultura = 0}
+
+personas = [isabel, mario, teresa]
+
+-- data Parques = parque
